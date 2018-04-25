b2c1defc32b05eff37e69e0afce88533d3e04f23
Ian Cordova <ian.a.cordova@gmail.com>
2018-04-25 18:00:27 -0400

    Completed parsing training data from data set

--- CipherSolver/CipherSolver.cs
+++ CipherSolver/CipherSolver.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace CipherSolver
 {
@@ -12,10 +13,8 @@ namespace CipherSolver
         // remove this after testing
         static void Main(string[] args)
         {
-            string word = "collection";
-            FindAllnGrams(word);
-
-            Console.ReadLine();
+            GetnGramsFromDataSet();
+            //Console.ReadLine();
         }
 
         /// <summary>
@@ -119,6 +118,112 @@ namespace CipherSolver
         private static bool IsEnglishLetter(char a_character)
         {
             return ((a_character >= 'A' && a_character <= 'Z') || (a_character >= 'a' && a_character <= 'z'));
+        }
+
+        /// <summary>
+        /// This is a method that is only called when creating our reference/training set of data.
+        /// ie it should be only called once, and not during normal project run time. This method
+        /// should probably be a separate entity in the form of a separate program or a script, but 
+        /// since it uses the same functionality that the main project does I am including it here 
+        /// to see everything that was done to achieve the finished project
+        /// </summary>
+        /// 
+        ///<returns>
+        /// void - but reads/writes to files
+        /// </returns>
+        /// 
+        /// <author>
+        /// Ian Cordova - 6:00pm 4/25/2018
+        /// </author>
+        private static void GetnGramsFromDataSet()
+        {
+            // dictionaries to store all n-grams and their number of occurances [n-gram, count]
+            Dictionary<string, int> bigrams = new Dictionary<string, int>();
+            Dictionary<string, int> trigrams = new Dictionary<string, int>();
+
+            // temporary storage for grams after they are found
+            List<string> grams = new List<string>();
+
+            string path = "C:\\Users\\icordova\\Source\\Repos\\CipherBreaker\\CipherSolver\\DataSet.csv";
+
+            // opening a filestream and reading it as a buffered stream is more efficient for large files
+            using (FileStream fs = File.Open(path, FileMode.Open))
+            using (BufferedStream bs = new BufferedStream(fs))
+            using (StreamReader sr = new StreamReader(bs))
+            {
+                string line;
+                string word;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    line = line.ToUpper();
+                    word = "";
+
+                    // iterate over the line char by char
+                    for(int i = 0; i < line.Length; ++i)
+                    {
+                        // begin saving letters to create a word
+                        if (IsEnglishLetter(line[i]))
+                        {
+                            word += line[i];
+                        }
+
+                        // we reached the end of a word or the end of a word at the end of a line
+                        if ((!IsEnglishLetter(line[i]) || i == line.Length - 1) && (word != "")) 
+                        {
+                            // save all bigrams to dictionary, increment duplicates
+                            grams = FindnGrams(2, word);
+                            foreach(string ngram in grams)
+                            {
+                                // increment bigram
+                                if(bigrams.ContainsKey(ngram))
+                                {
+                                    bigrams[ngram]++;
+                                }
+                                // first bigram
+                                else
+                                {
+                                    bigrams[ngram] = 1;
+                                }
+                            }
+                            grams.Clear();
+
+                            // save all trigrams to dictionary, increment duplicates
+                            grams = FindnGrams(3, word);
+                            foreach(string ngram in grams)
+                            {
+                                if(trigrams.ContainsKey(ngram))
+                                {
+                                    trigrams[ngram]++;
+                                }
+                                else
+                                {
+                                    trigrams[ngram] = 1;
+                                }
+                                
+                            }
+                            grams.Clear();
+                            word = "";
+                        }
+                    } 
+                } // end read line
+            } // end using
+
+            // write dictionaries to files
+            using (StreamWriter biGramFile = new StreamWriter("C:\\Users\\icordova\\Source\\Repos\\CipherBreaker\\CipherSolver\\Bigrams.txt"))
+            {
+                foreach(var entry in bigrams)
+                {
+                    biGramFile.WriteLine("[{0} {1}]", entry.Key, entry.Value);
+                }
+            }
+            using (StreamWriter triGramFile = new StreamWriter("C:\\Users\\icordova\\Source\\Repos\\CipherBreaker\\CipherSolver\\Trigrams.txt"))
+            {
+                foreach (var entry in trigrams)
+                {
+                    triGramFile.WriteLine("[{0} {1}]", entry.Key, entry.Value);
+                }
+            }
         }
     }
 }
